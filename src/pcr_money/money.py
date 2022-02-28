d1100d5d1bc1c2259c9e3c6578fcf5c74a9071cc
Luís Gustavo <luisgsilva270@gmail.com>
2022-02-28 18:49:38 +0000

    refactor: added the enums for the method types.

--- src/pcr_money/money.py
+++ src/pcr_money/money.py
@@ -1,28 +1,29 @@
 from requests import get
 import math
+from src.enum_method import MethodEnum
 
+roundv = MethodEnum.round.name
+truncation = MethodEnum.truncation.name
+floor = MethodEnum.floor.name
+ceil = MethodEnum.ceil.name
 
 class Money():
 
-    def __init__(self, value=0.0, decimals=2, coin_name='U$', method='round'):
+    def __init__(self, value=0.0, decimals=2, coin_name='U$', method =roundv):
         self.decimals = decimals
         self.coin_name = coin_name
         self.value = self.handle_value(value, decimals, method)
 
 
     @staticmethod
-    def handle_value(value, decimals, method='round'):
-        """
-        TODO think a better way to select and do this method
-        :method can be 'round', 'truncation', 'floor' and 'ceil
-        """
-        if(method == 'round'):
+    def handle_value(value, decimals, method =roundv):
+        if(method == roundv):
             return round(value, decimals)
-        elif(method == 'truncation' or method == 'floor'):
+        elif(method == truncation or method == floor):
             multiplier = pow(10, decimals)
             value_int = int(value * multiplier)
             return float(value_int/multiplier)
-        elif(method == 'ceil'):
+        elif(method == ceil):
             multiplier = pow(10, decimals)
             value_int = int(value * (10 * multiplier))
 
@@ -30,9 +31,11 @@ class Money():
                 value_int+=10
             value_int = int(value_int/10)
             return float(value_int/multiplier)
+        else:
+            return None
 
 
-    def applyRound(self, decimals, method='round'):
+    def applyRound(self, decimals, method=roundv):
         self.decimals = decimals
         self.value = self.handle_value(self.value, decimals, method)
 
